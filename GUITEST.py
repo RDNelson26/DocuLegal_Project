101fd5fd33bae08cc219d0e0dae22068af70b67a
Ryan <rdn26@yahoo.com>
2019-07-09 23:23:46 -0500

    working GUITEST

--- GUITEST.py
+++ GUITEST.py
@@ -1,51 +1,32 @@
 import os
 import sys
+from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtWidgets import QApplication, QDialog, QStackedLayout, QPushButton, QWidget, QMainWindow
 from PyQt5.uic import loadUi
 
+os.chdir (r"\Users\Ryan\PycharmProjects\untitled3\gui")
 
-
-
-wd = os.getcwd()
-os.chdir(wd + ('\gui'))
-
+class settings(QDialog):
+    def __init__(self):
+        super(settings, self).__init__()
+        loadUi("settings.ui", self)
 
 
 class mainpage(QDialog):
     def __init__(self):
         super(mainpage, self).__init__()
         loadUi('mainpage.ui', self)
-        self.pushButton.clicked.connect(self.settings)
+        self.pushButton_4.clicked.connect(self.move)
+        self.settings = settings()
+    def move(self):
+        self.settings.show()
+        self.hide()
 
-class settings(QDialog):
-    def __init__(self):
-        super(settings, self).__init__()
-        loadUi('settings.ui', self)
 
-class providernumber(QDialog):
-    def __init__(self):
-        super(providernumber, self).__init__()
-        loadUi('providernumber.ui', self)
-
-class LOR(QDialog):
-    def __init__(self):
-        super(LOR, self).__init__()
-        loadUi('LOR.ui', self)
-
-class LOP(QDialog):
-    def __init__(self):
-        super(LOP, self).__init__()
-        loadUi('LOP.ui', self)
-
-class RR(QDialog):
-    def __init__(self):
-        super(RR, self).__init__()
-        loadUi('RR.ui', self)
 
 app = QApplication(sys.argv)
 widget = mainpage()
 widget.show()
 sys.exit(app.exec_())
-
 
