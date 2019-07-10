3a02b50b453be2a76c758564bbe3a700bd5e071b
Ryan <rdn26@yahoo.com>
2019-07-09 23:22:02 -0500

    working GUITEST

--- GUITEST.py
+++ GUITEST.py
@@ -3,26 +3,25 @@ import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QDialog, QStackedLayout, QPushButton, QWidget, QMainWindow
-from PyQt5.uic import loadUi, stupUi
+from PyQt5.uic import loadUi
 
 os.chdir (r"\Users\Ryan\PycharmProjects\untitled3\gui")
 
-class settings(QtWidgets.QMainWindow)
-    def __init__(self, parent=None):
+class settings(QDialog):
+    def __init__(self):
         super(settings, self).__init__()
-        loadUi("settings.ui").self
+        loadUi("settings.ui", self)
 
 
-class mainpage(QtWidgets.QMainWindow):
+class mainpage(QDialog):
     def __init__(self):
         super(mainpage, self).__init__()
-        self.startmainpage
-        self.loadUi('mainpage.ui', self)
+        loadUi('mainpage.ui', self)
         self.pushButton_4.clicked.connect(self.move)
         self.settings = settings()
     def move(self):
         self.settings.show()
-
+        self.hide()
 
 
 
